96a1923356fe678543ff8406ad06bdb7d6735e5d
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2010-11-24 10:29:40 -0500

    Fixed up the emails

--- email.php
+++ email.php
@@ -1,50 +1,42 @@
-<?php
-require_once('template.php');
-page_header('SLUG Email Addresses');
-echo <<<END
-<div id="content3">
-Our group contains many people who volunteer their time and energy to make
-this a better group. Some of them are listed below. If you'd like to volunteer
-as well, <A HREF="./volunteer.php">let us know</A>!
-<CENTER>
-<P>
-President<BR>
-<A HREF="mailto:paulf@quillandmouse.com">Paul M. Foster</A><BR>
-<P>
-President Emeritus<BR>
-Henry White<BR>
-<P>
-Event Coordinator<BR>
-<A HREF="mailto:paulf@quillandmouse.com">Paul M. Foster</A><BR>
-<P>
-Meeting Coordinators<BR>
-<a href="mailto:paulf@quillandmouse.com">Paul Foster</a>, Dunedin Meetings<br>
-<a href="mailto:skkonn@gmail.com">Bill Davison</a>, Tampa Meetings<br>
-<P>
-Treasurer<BR>
-<A HREF="mailto:paulf@quillandmouse.com">Paul M. Foster</A><BR>
-<P>
-Webmaster<BR>
-<A HREF="mailto:paulf@quillandmouse.com">Paul M. Foster</A><BR>
-<P>
-List Maintainer<BR>
-<A HREF="mailto:paulf@quillandmouse.com">Paul M. Foster</A><BR>
-<p>
-Expansion Director<br>
-<P>
-Sponsor (website server space)<BR>
-<A HREF="mailto:slug-support@nks.net">Networked Knowledge Systems</A><BR>
-<P>
-Sponsor (list server)<BR>
-<A HREF="mailto:slug-support@nks.net">Networked Knowledge Systems</A><BR>
-<P>
-Logo design<BR>
-<A HREF="mailto:staff@quillandmouse.com">Nancy J. Foster/Quill &amp; Mouse Studios, Inc.</A><BR>
-<P>
-Help in other areas<BR>
-<A HREF="./volunteer.php">You!</A><BR>
-</CENTER>
-</div>
-END;
-page_footer();
+<?php $title = "SLUG Email Addresses";
+include("include/header.php");
 ?>
+<div id="content3" style="text-align: center;">
+	Our group contains many people who volunteer their time and energy to make this a better group. Some of them are listed below. If you'd like to volunteer as well, <a href="volunteer.php">let us know</a>!<br />
+	<br />
+	President<br />
+	<a href="mailto:president@suncoastlug.org">Dylan Hardison</a><br />
+	<br />
+	President Emeritus<br />
+	<br />
+	Event Coordinator<br />
+	<a href="mailto:paulf@quillandmouse.com">Paul M. Foster</a><br />
+	<br />
+	Meeting Coordinators<br />
+	<a href="mailto:president@suncoastlug.org">Dylan Hardison</a>, Pinellas Meetings<br />
+	<a href="mailto:skkonn@gmail.com">Bill Davison</a>, Tampa Meetings<br />
+	<br />
+	Treasurer<br />
+	<a href="mailto:treasury@suncoastlug.org">Bill Davison</a><br />
+	<br />
+	Webmaster<br />
+	<a href="mailto:president@suncoastlug.org">Dylan Hardison</a><br />
+	<br />
+	List Maintainer<br />
+	<a href="mailto:president@suncoastlug.org">Dylan Hardison</a><br />
+	<br />
+	Expansion Director<br />
+	<br />
+	Sponsor (website server space)<br />
+	<a href="mailto:spoonyspork@gmail.com">Spoony</a><br />
+	<br />
+	Sponsor (list server)<br />
+	<a href="mailto:president@suncoastlug.org">Dylan Hardison</a><br />
+	<br />
+	Logo design<br />
+	<a href="mailto:staff@quillandmouse.com">Nancy J. Foster/Quill &amp; Mouse Studios, Inc.</a><br />
+	<br />
+	Help in other areas<br />
+	<a href="volunteer.php">You!</a><br />
+</div>
+<?php include("include/footer.php"); ?>
